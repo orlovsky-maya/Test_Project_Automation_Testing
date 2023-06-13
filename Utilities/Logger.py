89bbcc546fb6a34a756ae7ffdc3ca1b4a2ace1cd
Maiia Orlovskaia <orlovsky-maya@mail.ru>
2023-06-13 14:35:52 +0400

    Updated Logger file

---
+++ Utilities/Logger.py
@@ -0,0 +1,34 @@
+import datetime
+import os
+
+
+class Logger:
+    now_date = datetime.datetime.utcnow().strftime("%Y_%m_%d.%H.%M.%S")
+    file_name = f"/home/maya/PycharmProjects/Building_test_project/Logs/log_{now_date}.log"
+
+    @classmethod
+    def write_log_to_file(cls, data: str):
+        with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
+            logger_file.write(data)
+
+    @classmethod
+    def add_start_step(cls, method: str):
+        test_name = os.environ.get('PYTEST_CURRENT_TEST')
+
+        data_to_add = f"\n-----\n"
+        data_to_add += f"Test: {test_name}\n"
+        data_to_add += f"Start time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"Start name method: {method}\n"
+        data_to_add += "\n"
+
+        cls.write_log_to_file(data_to_add)
+
+    @classmethod
+    def add_end_step(cls, url: str, method: str):
+
+        data_to_add = f"End time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"End name method: {method}\n"
+        data_to_add += f"URL: {url}\n"
+        data_to_add += f"\n-----\n"
+
+        cls.write_log_to_file(data_to_add)
