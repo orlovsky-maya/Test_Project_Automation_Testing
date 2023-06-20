2755be26128bce872f478bcd59610977337c2ae3
Maiia Orlovskaia <orlovsky-maya@mail.ru>
2023-06-20 10:39:54 +0400

    Updated Logger file

--- Utilities/Logger.py
+++ Utilities/Logger.py
@@ -3,16 +3,20 @@ import os
 
 
 class Logger:
-    now_date = datetime.datetime.utcnow().strftime("%Y_%m_%d.%H.%M.%S")
-    file_name = f"/home/maya/PycharmProjects/Building_test_project/Logs/log_{now_date}.log"
+    def __new__(cls, directory):
+        if not hasattr(cls, 'instance'):
+            logger = super(Logger, cls).__new__(cls)
+            now_date = datetime.datetime.utcnow().strftime("%Y_%m_%d.%H.%M.%S")
+            logger.file_name = f"{directory}/Logs/log_{now_date}.log"
 
-    @classmethod
-    def write_log_to_file(cls, data: str):
-        with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
+            cls.instance = logger
+        return cls.instance
+
+    def write_log_to_file(self, data: str):
+        with open(self.file_name, 'a', encoding='utf=8') as logger_file:
             logger_file.write(data)
 
-    @classmethod
-    def add_start_step(cls, method: str):
+    def add_start_step(self, method: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
         data_to_add = f"\n-----\n"
@@ -21,14 +25,12 @@ class Logger:
         data_to_add += f"Start name method: {method}\n"
         data_to_add += "\n"
 
-        cls.write_log_to_file(data_to_add)
+        self.write_log_to_file(data_to_add)
 
-    @classmethod
-    def add_end_step(cls, url: str, method: str):
-
+    def add_end_step(self, url: str, method: str):
         data_to_add = f"End time: {str(datetime.datetime.now())}\n"
         data_to_add += f"End name method: {method}\n"
         data_to_add += f"URL: {url}\n"
         data_to_add += f"\n-----\n"
 
-        cls.write_log_to_file(data_to_add)
+        self.write_log_to_file(data_to_add)
