5df0d6af5685d16564cfa04e8affb17856269c9a
Maiia Orlovskaia <orlovsky-maya@mail.ru>
2023-06-20 10:33:06 +0400

    Added logger parameter to BasePage Changed directory for screenshots

--- Base/base_page.py
+++ Base/base_page.py
@@ -1,12 +1,14 @@
 import datetime
+from urllib import request
 
 
 class BasePage:
 
-    def __init__(self, browser, url, timeout=30):
+    def __init__(self, browser, url, logger, timeout=30):
         self.browser = browser
         self.timeout = timeout
         self.url = url
+        self.logger = logger
 
     def open(self):
         self.browser.get(self.url)
@@ -19,10 +21,10 @@ class BasePage:
         value_word = word.text
         assert value_word == result, "Incorrect result word"
 
-    def get_screenshot(self):
+    def get_screenshot(self, directory):
         now_date = datetime.datetime.utcnow().strftime("%Y_%m_%d.%H.%M.%S")
         name_screenshot = f'finish_scr_{now_date}.png'
-        self.browser.save_screenshot(f'/home/maya/PycharmProjects/Building_test_project/Screen/{name_screenshot}')
+        self.browser.save_screenshot(f'{directory}/Screen/{name_screenshot}')
 
     def assert_url(self, result):
         get_url = self.browser.current_url
